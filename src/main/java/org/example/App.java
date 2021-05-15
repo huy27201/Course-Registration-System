0c1db07bcd3171932de6d2df2368ba3f64821304
huy27201 <61152052+huy27201@users.noreply.github.com>
2021-05-15 18:13:35 +0700

    Commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -23,7 +23,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         this.stage = stage;
-        Parent root = loadFXML("Login");
+        Parent root = loadFXML("TeacherDashboard");
         scene = new Scene(root);
         root.setOnMousePressed(new EventHandler<MouseEvent>() {
                 @Override
