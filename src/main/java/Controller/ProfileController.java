5f1586e9916158d27a35ab1f5f309462ff9ea9d7
huy27201 <61152052+huy27201@users.noreply.github.com>
2021-06-03 21:23:40 +0700

    Update more and more...

---
+++ src/main/java/Controller/ProfileController.java
@@ -0,0 +1,46 @@
+package Controller;
+
+import POJO.Teacher;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+
+public class ProfileController {
+    @FXML private TextField id;
+    @FXML private TextField firstName;
+    @FXML private TextField lastName;
+    @FXML private DatePicker dateOfBirth;
+    @FXML ToggleGroup gender;
+    @FXML RadioButton male;
+    @FXML RadioButton female;
+    @FXML ComboBox classLists;
+    @FXML private TextField account;
+    @FXML private TextField password;
+
+    public void setUser(Teacher tch) {
+        classLists.setDisable(true);
+        dateOfBirth.setDisable(true);
+        id.setText(tch.getId());
+        firstName.setText(tch.getFirstName());
+        lastName.setText(tch.getLastName());
+        if (tch.getSex().equals("Nam")) male.setSelected(true);
+        else female.setSelected(true);
+        account.setText(tch.getAccountByAccount().getAccountId());
+        password.setText(tch.getAccountByAccount().getPassword());
+    }
+    @FXML
+    public void onSaveProfile(Event event) {
+        Stage dialog = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        dialog.close();
+    }
+    @FXML
+    public void onKeySave(KeyEvent event) {
+        if (event.getCode().equals(KeyCode.ENTER)) {
+            onSaveProfile(event);
+        }
+    }
+}
